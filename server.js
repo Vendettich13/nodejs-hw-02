3fd70e1810669e0a87d8301af075707109b52405
Victor Litvynchuk <vendettich13@icloud.com>
2023-02-22 14:44:39 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 const app = require("./app");
-const DB_HOST =
-  "mongodb+srv://Rick:RickSanchez013@cluster0.ds26vl4.mongodb.net/db-contacts?retryWrites=true&w=majority";
+console.log(process.env);
+
+const { DB_HOST } = process.env;
 
 mongoose.set("strictQuery", true);
 
@@ -9,7 +10,7 @@ mongoose
   .connect(DB_HOST)
   .then(() =>
     app.listen(3000, () => {
-      console.log("Server running. Use our API on port: 3000");
+      console.log("Database connection successful");
     })
   )
   .catch((err) => {
