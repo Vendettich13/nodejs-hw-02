3e76b0e346636e9ea2c515b73e13350bbb3fa266
Victor Litvynchuk <vendettich13@icloud.com>
2023-02-22 14:02:14 +0200

    add mongoose

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+const app = require("./app");
+const DB_HOST =
+  "mongodb+srv://Rick:RickSanchez013@cluster0.ds26vl4.mongodb.net/db-contacts?retryWrites=true&w=majority";
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() =>
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    })
+  )
+  .catch((err) => {
+    console.log(err.message);
+    process.exit(1);
+  });
